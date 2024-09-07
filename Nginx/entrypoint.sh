41576d13e2abddee632e8e0405291d780747e348
p3wc0 <p3wc0@P3WC0>
2024-09-07 17:34:34 -0500

    done

---
+++ Nginx/entrypoint.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+if [ -d "/etc/letsencrypt/live/${DOMAIN}.duckdns.org" ]; then
+	echo "OK"
+  envsubst '${DOMAIN}' < ./nginx-rewrite.conf > /etc/nginx/sites-enabled/default
+  service php8.2-fpm start 
+	nginx -g 'daemon off;'
+else
+  echo "No certificate"
+  exit 1
+fi
